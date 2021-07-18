4a071989473d6c6ab1b30a7ec0980777c07f23d9
Cuber01 <johnmallard631@gmail.com>
2021-07-18 20:27:34 +0200

    yeeted code into classes

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 
 #include <iostream>
 #include <cstring>
-#include <fstream>
-using std::ifstream;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
+#include "shader.cpp"
+#include "glfw.cpp"
+#include "utils.cpp"
+
 
 // set up vertex data (and buffer(s)) and configure vertex attributes
 float vertices[] = {
@@ -19,114 +19,18 @@ float vertices[] = {
 
 };
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
 
-GLFWwindow* window;
+
 int success;
 char infoLog[512];
 
 const char* fragmentShaderPath = "./res/fragment_shader.fs";
 const char* vertexShaderPath = "./res/vertex_shader.vs";
 
-#define BUFFER_SIZE 2000;
+#define BUFFER_SIZE 2000
 
 char sbuffer[BUFFER_SIZE];
 
-int _glfwInit()
-{
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
-    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    return 0;
-
-}
-
-unsigned int compileShader(int type, const char* shaderSource)
-{
-    unsigned int shader = glCreateShader(type);
-    glShaderSource(shader, 1, &shaderSource, NULL);
-    glCompileShader(shader);
-    // check for shader compile errors
-    
-
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    return shader;
-}
-
-unsigned int makeShaderProgram(unsigned int shaders[1])
-{
-    unsigned int shaderProgram = glCreateProgram();
-    for (int i = 0; i < 2; i++)
-    {
-        glAttachShader(shaderProgram, shaders[i]);
-    }
-    
-    glLinkProgram(shaderProgram);
-
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    for (int i = 0; i < 2; i++)
-    {
-        glDeleteShader(shaders[i]);
-    }
-
-    return shaderProgram;
-}
-
-char data[2000];//todo: poprawic
-
-int readFile( const char* fileName, char* buffer, unsigned int maxFileSize )
-{
-    unsigned int n;
-    ifstream ifs(fileName);
-    std::cout << std::endl << "Read FILE: " << fileName << std::endl;
-    // ifs.open(fileName);
-    ifs.read( buffer, maxFileSize );
-    n = ifs.gcount();
-    ifs.close();
-
-    return n;
-}
-
-void printFile( const char* data, unsigned int n ){
-    std::cout << std::endl << "--- Readed FILE Start: ----------------------------------------------" << std::endl;
-    for( int i=0; i< n; i++ ){
-        std::cout << data[i];
-    }
-    std::cout << std::endl << "--- Readed FILE End: ----------------------------------------------" << std::endl;    
-}
 
 
 
@@ -150,21 +54,9 @@ unsigned int processVerticesData()
     return VAO;
 }
 
-
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-void processInput(GLFWwindow *window)
+void Render()
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+    
 }
 
 
@@ -172,22 +64,31 @@ int main()
 {
     int n;
 
-    _glfwInit();
+    CShaderHandler ShaderHandler;
+    CGlfwHandler GlfwHandler;
+    CUtils Utils;
+
+    GlfwHandler._glfwInit();
+
+   
     
     memset(sbuffer, 0, sizeof(sbuffer));
-    n = readFile(fragmentShaderPath,sbuffer, BUFFER_SIZE );
-    printFile( sbuffer,n);
-    std::cout << std::endl << "--- Compile: ----------------------------------------------" << std::endl;    
-    unsigned int fragmentShader = compileShader(GL_FRAGMENT_SHADER , sbuffer );
+    n = Utils.readFile(fragmentShaderPath,sbuffer, BUFFER_SIZE );
+
+   // std::cout << std::endl << "--- Compile: ----------------------------------------------" << std::endl;  
+
+    unsigned int fragmentShader = ShaderHandler.compileShader(GL_FRAGMENT_SHADER , sbuffer );
 
     memset(sbuffer, 0, sizeof(sbuffer));    
-    n = readFile(vertexShaderPath,sbuffer, BUFFER_SIZE );
-    printFile( sbuffer,n);        
-    std::cout << std::endl << "--- Compile: ----------------------------------------------" << std::endl;        
-    unsigned int vertexShader  = compileShader(GL_VERTEX_SHADER , sbuffer );
+    n = Utils.readFile(vertexShaderPath,sbuffer, BUFFER_SIZE );
+    
+
+    std::cout << std::endl << "--- Compile: ----------------------------------------------" << std::endl;  
+
+    unsigned int vertexShader  = ShaderHandler.compileShader(GL_VERTEX_SHADER , sbuffer );
 
     unsigned int shaders[] = {fragmentShader, vertexShader};
-    unsigned int shaderProgram = makeShaderProgram(shaders);
+    unsigned int shaderProgram = ShaderHandler.makeShaderProgram(shaders);
 
     unsigned int VAO = processVerticesData();
 
@@ -195,10 +96,10 @@ int main()
     glUseProgram(shaderProgram);
 
     // render loop
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(GlfwHandler.window))
     {
         // input
-        processInput(window);
+        GlfwHandler.processInput(GlfwHandler.window);
 
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -209,7 +110,7 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(GlfwHandler.window);
         glfwPollEvents();
     }
 
---
+++ src/shader.cpp
@@ -0,0 +1,57 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "main.hpp"
+
+
+class CShaderHandler {       
+    public:             
+
+    unsigned int compileShader(int type, const char* shaderSource)
+    {
+        unsigned int shader = glCreateShader(type);
+        glShaderSource(shader, 1, &shaderSource, NULL);
+        glCompileShader(shader);
+        // check for shader compile errors
+
+
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        return shader;
+    }
+
+
+    unsigned int makeShaderProgram(unsigned int shaders[1])
+    {
+
+    unsigned int shaderProgram = glCreateProgram();
+    for (int i = 0; i < 2; i++)
+    {
+        glAttachShader(shaderProgram, shaders[i]);
+    }
+    
+    glLinkProgram(shaderProgram);
+
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        glDeleteShader(shaders[i]);
+    }
+
+    return shaderProgram;
+}
+
+      
+};
